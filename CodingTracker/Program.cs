c3fb8dd91c5f4e621742d257b771afd4afac0d6a
zsmjames@gmail.com <zsmjames@gmail.com>
2025-05-04 12:53:38 +0300

    Initial Commit

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! OnemoremodXDD"); Console.WriteLine("Hello, C# Academy! OnemoremodXDD"); 
+﻿void MyFirstMethod()
+{
+    //Do something
+}
+
+void MySecondMethod()
+{
+    //Do something
+}
+
+void MySecondMethod()
+{
+    //Do something
+}
